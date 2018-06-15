f12ef607f18dddb3c31dea3ccdadc4ed96990fd2
jisuanji-PC\jisuanji <1272768836@qq.com>
2018-06-15 22:41:10 +0800

    提供基本工具类js，ajax open封装

---
+++ eshopApp/js/util.js
@@ -0,0 +1,40 @@
+//打开窗口
+function openWindow(url, id, data) {
+	mui.openWindow({
+		url: url,
+		id: id,
+		preload: true,
+		show: {
+			aniShow: 'pop-in'
+		},
+		styles: {
+			popGesture: 'hide'
+		},
+		extras: data,
+		waiting: {
+			autoShow: false
+		}
+	});
+}
+
+//ajax基本方法
+function ajax(url, data, callback) {
+	mui.ajax(url, {
+		data: data,
+		type: 'post',
+		timeout: 10000,
+		dataType: 'json',
+		beforeSend: function() {
+			plus.nativeUI.showWaiting("请稍后...");
+		},
+		success: function(res) {
+			plus.nativeUI.closeWaiting();
+			callback(res);
+		},
+		error: function(xhr, type, errorThrown) {
+			callback(null);
+			plus.nativeUI.closeWaiting();
+			mui.toast("出现异常:" + type);
+		}
+	});
+}
